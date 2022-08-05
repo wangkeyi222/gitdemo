8506c4686bb770bfd2a35efbb894093c395de025
wangkeyi222 <240200811@qq.com>
2022-08-05 10:16:50 +0800

    dev分支

--- src/main/java/com/wky/Test01.java
+++ src/main/java/com/wky/Test01.java
@@ -3,7 +3,7 @@ package com.wky;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("1");
-        System.out.println("dev");
+        System.out.println("aa");
         System.out.println("2022-0805");
     }
 }
